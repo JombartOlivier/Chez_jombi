61446987a39cd10e0893483e22e1d037a53f9a5a
JombartOlivier <olivier.jombart@isen.yncrea.fr>
2017-02-13 15:08:10 +0100

    Classe fille

---
+++ Chezjombi/src/chezjombi/Client.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chezjombi;
+
+/**
+ *
+ * @author Olivier
+ */
+public class Client extends Humain {
+    
+    public Client(String prenom, String surnom, float porteMonaie, int cotePopularite) {
+        super(prenom, surnom, porteMonaie, cotePopularite);
+    }
+    
+}
---
+++ Chezjombi/src/chezjombi/Patron.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chezjombi;
+
+/**
+ *
+ * @author Olivier
+ */
+public class Patron extends Humain {
+    
+    public Patron(String prenom, String surnom, float porteMonaie, int cotePopularite) {
+        super(prenom, surnom, porteMonaie, cotePopularite);
+    }
+    
+}
