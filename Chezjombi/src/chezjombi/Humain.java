f82960ec90bbadf24b0dbd4a5d05a45338123de3
ArthurDuytschaever <arthur.duytschaever@isen.yncrea.fr>
2017-02-13 14:58:51 +0100

    Creation de la classe mere Humain et ajout d'attriubue

--- Chezjombi/src/chezjombi/Humain.java
+++ Chezjombi/src/chezjombi/Humain.java
@@ -1,7 +1,19 @@
 package chezjombi;
 
+
 /**
  * Created by ISEN on 13/02/2017.
  */
 public class Humain {
+    
+    protected String prenom;
+    protected String surnom;
+    protected float porteMonaie;
+    protected int cotePopularite;
+    protected String crieSignificatif;
+    
+    public Humain(String prenom, String surnom, float porteMonaie, int cotePopularite){
+        
+    }
+    
 }
