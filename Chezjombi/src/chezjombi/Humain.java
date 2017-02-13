2c74f69765ebaa99db38a4d99cdb8df6310ce456
ArthurDuytschaever <arthur.duytschaever@isen.yncrea.fr>
2017-02-13 15:07:32 +0100

    Mise a jour du construteur

--- Chezjombi/src/chezjombi/Humain.java
+++ Chezjombi/src/chezjombi/Humain.java
@@ -6,14 +6,40 @@ package chezjombi;
  */
 public class Humain {
     
-    protected String prenom;
+    private final String prenom;
     protected String surnom;
     protected float porteMonaie;
     protected int cotePopularite;
     protected String crieSignificatif;
     
-    public Humain(String prenom, String surnom, float porteMonaie, int cotePopularite){
+    public Humain(String prenom, String surnom, float porteMonaie, int cotePopularite,String crieSignificatif){
+        
+        this.prenom = prenom;
+        this.surnom = surnom;
+        this.porteMonaie = porteMonaie;
+        this.cotePopularite = cotePopularite;
+        this.crieSignificatif = crieSignificatif;
         
     }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getSurnom() {
+        return surnom;
+    }
+
+    public float getPorteMonaie() {
+        return porteMonaie;
+    }
+
+    public String getCrieSignificatif() {
+        return crieSignificatif;
+    }
+    
+   
+    
+    
     
 }
