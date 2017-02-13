a137586ee3d27a6a84e3265646f64afb79c14709
ArthurDuytschaever <arthur.duytschaever@isen.yncrea.fr>
2017-02-13 15:13:53 +0100

    Ajout de la java doc a la classe humain

--- Chezjombi/src/chezjombi/Humain.java
+++ Chezjombi/src/chezjombi/Humain.java
@@ -2,8 +2,12 @@ package chezjombi;
 
 
 /**
- * Created by ISEN on 13/02/2017.
+ * Classe Mere Humain
+ * Created by Arthur Duytschaever on 13/02/2017.
+ * @author  Olivie Jombart, Arthur Duytschaever
+ * @version v1.0
  */
+
 public class Humain {
     
     private final String prenom;
@@ -12,6 +16,14 @@ public class Humain {
     protected int cotePopularite;
     protected String crieSignificatif;
     
+/**
+ * Construteur de la classe humain  
+ * @param prenom
+ * @param surnom
+ * @param porteMonaie
+ * @param cotePopularite
+ * @param crieSignificatif 
+ */
     public Humain(String prenom, String surnom, float porteMonaie, int cotePopularite,String crieSignificatif){
         
         this.prenom = prenom;
